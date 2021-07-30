62fc4e4bc1cae538a81bd6770a23ef952ef823d8
戴海洋 <1037306928@qq.com>
2021-07-30 10:27:47 +0800

    axios文件进一步加强

--- Axios/request.ts
+++ Axios/request.ts
@@ -1,15 +1,66 @@
 import Axios from 'axios'
 import { AxiosResponse, AxiosRequestConfig } from 'axios'
 import { ElMessage } from 'element-plus'
+import http from 'http'
+import https from 'https'
+import qs from 'qs'
 
-export const axios = Axios.create({
+const config: AxiosRequestConfig = {
 	baseURL: '',
 	timeout: 5000,
 	withCredentials: true,
+	responseType: 'json',
+	// xsrf 设置
+	xsrfCookieName: 'XSRF-TOKEN',
+	xsrfHeaderName: 'X-XSRF-TOKEN',
+	// 最多转发数，用于node.js
+	maxRedirects: 5,
+	// 最大响应数据大小
+	maxContentLength: 2000,
 	headers: {
 		'Content-Type': 'application/json;charset=UTF-8'
+	},
+	// 请求后的数据处理
+	transformResponse: [
+		function (data: AxiosResponse) {
+			return data
+		}
+	],
+	// 查询对象序列化函数
+	paramsSerializer: function (params: any) {
+		return qs.stringify(params)
+	},
+	// 自定义错误状态码范围
+	validateStatus: function (status: number) {
+		return status >= 200 && status < 300
+	},
+	// 用于node.js
+	httpAgent: new http.Agent({ keepAlive: true }),
+	httpsAgent: new https.Agent({ keepAlive: true })
+}
+
+export const axios = Axios.create(config)
+
+// 取消重复请求
+let pending: Array<{
+	url: string
+	cancel: Function
+}> = []
+
+const cancelToken = axios.CancelToken
+const removePending = (config: AxiosRequestConfig) => {
+	for (let p in pending) {
+		let item: any = p
+		let list: any = pending[p]
+		// 当前请求在数组中存在时执行函数体
+		if (list.url === config.url + '&request_type=' + config.method) {
+			// 执行取消操作
+			list.cancel()
+			// 从数组中移除记录
+			pending.splice(item, 1)
+		}
 	}
-})
+}
 
 // 请求拦截器
 axios.interceptors.request.use(
@@ -18,6 +69,10 @@ axios.interceptors.request.use(
 		 * 根据你的项目实际情况来对 config 做处理
 		 * 这里对 config 不做任何处理，直接返回
 		 */
+		removePending(response)
+		response.cancelToken = new cancelToken((c: any) => {
+			pending.push({ url: response.url + '&request_type=' + response.method, cancel: c })
+		})
 		return response
 	},
 	(error: any) => {
@@ -31,11 +86,8 @@ axios.interceptors.response.use(
 		* 根据你的项目实际情况来对 response 和 error 做处理
 		* 这里对 response 和 error 不做任何处理，直接返回
 		*/
-		if (typeof response.status && response.status >= 200 && response.status < 400) {
-			return response
-		} else {
-			console.log(response, '状态码不在200-400内')
-		}
+		removePending(response.config)
+		return response
 	},
 	(error: any) => {
 		if (error.response && error.response.data) {
