ecbe5962185fdec83faf9e756547aca6ff53dd35
戴海洋 <1037306928@qq.com>
2021-08-01 18:23:36 +0800

    修改axios中文件取消重复请求的机制

--- Axios/request.ts
+++ Axios/request.ts
@@ -1,15 +1,18 @@
-import Axios from 'axios'
+import axios from 'axios'
 import { AxiosResponse, AxiosRequestConfig } from 'axios'
-import { ElMessage } from 'element-plus'
 import http from 'http'
 import https from 'https'
-import qs from 'qs'
+import Qs from 'qs'
 
-const config: AxiosRequestConfig = {
+export const UserAgent =
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'
+export const ContentType = 'application/x-www-form-urlencoded'
+
+export const request = axios.create({
 	baseURL: '',
 	timeout: 5000,
 	withCredentials: true,
-	responseType: 'json',
+	responseType: 'json', //文档设置为document自动转化为DOM、text为文字、blob等
 	// xsrf 设置
 	xsrfCookieName: 'XSRF-TOKEN',
 	xsrfHeaderName: 'X-XSRF-TOKEN',
@@ -20,84 +23,95 @@ const config: AxiosRequestConfig = {
 	headers: {
 		'Content-Type': 'application/json;charset=UTF-8'
 	},
+
+	// 查询对象序列化函数
+	paramsSerializer: function(params: any) {
+		return Qs.stringify(params, { arrayFormat: 'brackets' })
+	},
+
 	// 请求后的数据处理
 	transformResponse: [
-		function (data: AxiosResponse) {
+		function(data: AxiosResponse) {
 			return data
 		}
 	],
-	// 查询对象序列化函数
-	paramsSerializer: function (params: any) {
-		return qs.stringify(params)
-	},
+
 	// 自定义错误状态码范围
-	validateStatus: function (status: number) {
-		return status >= 200 && status < 300
+	validateStatus: function(status: number) {
+		return status >= 200 && status < 400
 	},
+
 	// 用于node.js
 	httpAgent: new http.Agent({ keepAlive: true }),
 	httpsAgent: new https.Agent({ keepAlive: true })
-}
 
-export const axios = Axios.create(config)
-
-// 取消重复请求
-let pending: Array<{
-	url: string
-	cancel: Function
-}> = []
-
-const cancelToken = axios.CancelToken
-const removePending = (config: AxiosRequestConfig) => {
-	for (let p in pending) {
-		let item: any = p
-		let list: any = pending[p]
-		// 当前请求在数组中存在时执行函数体
-		if (list.url === config.url + '&request_type=' + config.method) {
-			// 执行取消操作
-			list.cancel()
-			// 从数组中移除记录
-			pending.splice(item, 1)
-		}
-	}
-}
+	// proxy: {
+	// 	host: '127.0.0.1',
+	// 	port: 9000
+	// }
+})
 
 // 请求拦截器
-axios.interceptors.request.use(
-	(response: AxiosRequestConfig) => {
-		/**
-		 * 根据你的项目实际情况来对 config 做处理
-		 * 这里对 config 不做任何处理，直接返回
-		 */
-		removePending(response)
-		response.cancelToken = new cancelToken((c: any) => {
-			pending.push({ url: response.url + '&request_type=' + response.method, cancel: c })
-		})
-		return response
+request.interceptors.request.use(
+	(config: AxiosRequestConfig) => {
+		removePendingRequest(config) // 检查是否存在重复请求，若存在则取消已发的请求
+		addPendingRequest(config) // 把当前请求信息添加到pendingRequest对象中
+		return config
 	},
 	(error: any) => {
-		return Promise.reject(error)
-	})
-
-// 响应拦截器,响应拦截器中添加响应错误状态码、数据的判断
-axios.interceptors.response.use(
-	(response: AxiosResponse) => {
-		/**
-		* 根据你的项目实际情况来对 response 和 error 做处理
-		* 这里对 response 和 error 不做任何处理，直接返回
-		*/
-		removePending(response.config)
-		return response
-	},
-	(error: any) => {
-		if (error.response && error.response.data) {
-			const code = error.response.status
-			const msg = error.response.data.message
-			ElMessage.error(`状态码: ${code}, 错误消息: ${msg}`)
-			console.error(`[Axios 错误]`, error.response)
-		} else {
-			ElMessage.error(`${error}`)
-		}
 		return Promise.reject(error)
 	}
 )
+
+// 响应拦截器,响应拦截器中添加响应错误状态码、数据的判断
+request.interceptors.response.use(
+	(res: AxiosResponse) => {
+		removePendingRequest(res.config) // 从pendingRequest对象中移除请求
+		return res
+	},
+	(err: any) => {
+		removePendingRequest(err.config || {}) // 从pendingRequest对象中移除请求
+		if (axios.isCancel(err)) {
+			console.log('已取消的重复请求：' + err.message)
+		} else {
+			// 添加异常处理
+		}
+		//根据上面的自定义状态码抛出错误
+		return Promise.reject(err)
+	}
+)
+
+/**
+ * @描述 取消重复请求
+ * @作者 HY
+ * @时间 2021-08-01 18:19
+ */
+const pendingRequest = new Map()
+
+// 生成取消重复请求Map中的key
+function generateReqKey(config) {
+	const { method, url, params, data } = config
+	return [method, url, Qs.stringify(params), Qs.stringify(data)].join('&')
+}
+
+// Map中没有就添加进去
+function addPendingRequest(config) {
+	const requestKey = generateReqKey(config)
+	config.cancelToken =
+		config.cancelToken ||
+		new axios.CancelToken((cancel) => {
+			if (!pendingRequest.has(requestKey)) {
+				pendingRequest.set(requestKey, cancel)
+			}
+		})
+}
+// 删除重复请求
+function removePendingRequest(config) {
+	const requestKey = generateReqKey(config)
+
+	if (pendingRequest.has(requestKey)) {
+		const cancelToken = pendingRequest.get(requestKey)
+		cancelToken(requestKey)
+		pendingRequest.delete(requestKey)
+	}
+}
