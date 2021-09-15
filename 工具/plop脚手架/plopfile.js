fe0436d6f12ba12732bea7c9c6ba3d26444f19fc
戴海洋 <1037306928@qq.com>
2021-09-15 11:29:23 +0800

    style:plop注释修改

--- 工具/plop脚手架/plopfile.js
+++ 工具/plop脚手架/plopfile.js
@@ -1,3 +1,16 @@
+// camelCase: changeFormatToThis
+// snakeCase: change_format_to_this
+// dashCase/kebabCase: change-format-to-this
+// dotCase: change.format.to.this
+// pathCase: change/format/to/this
+// properCase/pascalCase: ChangeFormatToThis
+// lowerCase: change format to this
+// sentenceCase: Change format to this,
+// constantCase: CHANGE_FORMAT_TO_THIS
+// titleCase: Change Format To This
+
+// 运行命令：plop
+
 module.exports = (plop) => {
     plop.setGenerator('页面', {
         // 描述
