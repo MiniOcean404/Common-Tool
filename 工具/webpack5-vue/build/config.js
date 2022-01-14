6319b8e52deb21e0ac73b561e4da2d9810484033
戴海洋 <1037306928@qq.com>
2022-01-14 10:48:54 +0800

    修改webpack-config文件

--- 工具/webpack5-vue/build/config.js
+++ 工具/webpack5-vue/build/config.js
@@ -1,7 +1,7 @@
 const { resolve } = require('./utils');
 const env = process.env.NODE_ENV;
 
-const devMode = env === 'development' || 'online';
+const devMode = env === 'development' || env === 'online';
 
 const getDotEnv = () => {
 	const dotEnv = require('dotenv').config({ path: resolve(`.env.${env}`) }).parsed || {};
