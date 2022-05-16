7fc659e20c35c134d3bbe23048eca9cb9c7c9ca4
MiniOcean404 <1037306928@qq.com>
2022-05-16 22:31:03 +0800

    style: 格式化静态资源服务

--- 工具/静态服务器/index.js
+++ 工具/静态服务器/index.js
@@ -1,21 +1,21 @@
-const fs = require("fs");
-const http = require("http");
-const https = require("https");
-const express = require("express");
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
+const express = require('express')
 
 const options = {
-  key: fs.readFileSync("./https-pem/private.pem"),
-  cert: fs.readFileSync("./https-pem/cert.crt"),
-};
+  key: fs.readFileSync('./https-pem/private.pem'),
+  cert: fs.readFileSync('./https-pem/cert.crt'),
+}
 
 //创建web服务器
-const app = express();
+const app = express()
 //托管静态资源
-app.use(express.static("./dist"));
+app.use(express.static('./web'))
 
 http.createServer(app).listen(80, () => {
-  console.log("http服务器-----http://127.0.0.1");
-});
+  console.log('http服务器-----http://127.0.0.1')
+})
 https.createServer(options, app).listen(443, () => {
-  console.log("https服务器-----https://127.0.0.1");
-});
+  console.log('https服务器-----https://127.0.0.1')
+})
